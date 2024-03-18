bb76e90684089926351e4109c214ba1fbd638878
Guillermo Anguiano <jesus2001guillermo2@gmail.com>
2024-03-17 21:51:35 -0700

    o

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import "dotenv/config";
 import express from "express";
-import cors from 'cors';
-import Routes from './routes';
+import cors from "cors";
+import Routes from "./routes";
+import db from './config/mongo';
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 app.use(cors());
 app.use(Routes);
+db().then(() => console.log("Connection ready"))
 
 app.listen(PORT, () => {
   console.log(`Server iniciado en el server ${PORT}`);
-})
-
+});
---
+++ src/controllers/task.ts
@@ -0,0 +1,19 @@
+export const getTask = (req, res) => {
+
+}
+
+export const getTasks = (req, res) => {
+
+}
+
+export const updateTaks = (req, res) => {
+
+}
+
+export const postTask = (req, res) => {
+
+}
+
+export const deleteTask = (req, res) => {
+
+}
